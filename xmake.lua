da6eaf8d30ef599d076ea6ce6d6b8a734a74de5a
XyKong <30373236+traversebitree@users.noreply.github.com>
2024-02-06 00:49:20 +0800

    Update xmake.lua to adopt Windows by force toolchain platform set to "mingw"
    Update xmake.lua to adopt Windows by force toolchain platform set to "mingw"

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,17 @@
 add_rules("mode.debug", "mode.release")
+add_rules("set_mingw_for_zig_on_windows")
 
 add_requires("zig")
+
+rule("set_mingw_for_zig_on_windows")
+do
+    before_build(function (target)
+        if target:toolchains()[1]:config("zig") and target:toolchains()[1]:config("plat")=="windows" then
+           target:toolchains()[1]:plat_set("mingw")
+        end
+    end)
+end 
+rule_end()
 
 target("zigcc-scaffold")
     set_kind("binary")
